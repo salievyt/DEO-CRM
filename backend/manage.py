2cfb61d762f7a3490b4205562405dad743ae3170
salievyt <salievyt@gmail.com>
2026-09-19 17:41:59 +0600

    fix Vercel Django settings

--- backend/manage.py
+++ backend/manage.py
@@ -6,7 +6,10 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+    default_settings = (
+        "config.settings.production" if os.environ.get("VERCEL") else "config.settings.local"
+    )
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", default_settings)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
